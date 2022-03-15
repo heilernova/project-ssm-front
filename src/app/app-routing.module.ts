c30a6fe8a87ca590f9d65b127e8c4d186d14b3a3
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-15 09:08:20 -0500

    Se modificaron las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {
     path: '', component:BodyComponent,
     children: [
-      { path: 'atencion-a-la-comunidad', loadChildren:()=>import('./pages/pg-attention-community/pg-attention-community.module').then(m=>m.PgAttentionCommunityModule) },
+      { path: 'servicio-de-atencion-al-ciudadano', loadChildren:()=>import('./pages/pg-attention-community/pg-attention-community.module').then(m=>m.PgAttentionCommunityModule) },
       { path: 'configuraciones', loadChildren:()=>import('./pages/pg-config/pg-config.module').then(m=>m.PgConfigModule) }
     ]
   },
