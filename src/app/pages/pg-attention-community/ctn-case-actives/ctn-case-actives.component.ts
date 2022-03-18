0c96e025de68d6cd71fba34864acadd0a6c62dae
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-18 11:47:17 -0500

    Se creo el componente de casos activos

--- src/app/pages/pg-attention-community/ctn-case-actives/ctn-case-actives.component.ts
+++ src/app/pages/pg-attention-community/ctn-case-actives/ctn-case-actives.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ICase } from '../interfaces/ICase';
+import { CasesService } from '../services/cases.service';
 
 @Component({
   selector: 'app-ctn-case-actives',
   templateUrl: './ctn-case-actives.component.html',
   styleUrls: ['./ctn-case-actives.component.scss']
 })
-export class CtnCaseActivesComponent implements OnInit {
+export class CtnCaseActivesComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  dataSource =  new MatTableDataSource<ICase>([]);
+  displayColums:string[] = ['date', 'dni', 'name', 'actions'];
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(
+    private _cases:CasesService
+  ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
   }
 
+  ngOnInit(): void {
+    this.updateList();
+  }
+
+
+  updateList(){
+    this._cases.onGetCases().subscribe({
+      next: data => {
+        this.dataSource.data = data;
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    // if (this.dataSource.paginator) {
+    //   this.dataSource.paginator.firstPage();
+    // }
+  }
 }
--- src/app/pages/pg-attention-community/ctn-case-actives/ctn-case-actives.module.ts
+++ src/app/pages/pg-attention-community/ctn-case-actives/ctn-case-actives.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CtnCaseActivesComponent } from './ctn-case-actives.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 
 
@@ -9,7 +14,12 @@ import { CtnCaseActivesComponent } from './ctn-case-actives.component';
     CtnCaseActivesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule
   ]
 })
 export class CtnCaseActivesModule { }
