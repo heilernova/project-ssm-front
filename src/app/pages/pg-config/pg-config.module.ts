d06606b5f2f064f2f323a0fd56e6c1b1298f0885
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-03-19 11:13:08 -0500

    Se elimino la sección acterios de EPS

--- src/app/pages/pg-config/pg-config.module.ts
+++ src/app/pages/pg-config/pg-config.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PgConfigComponent } from './pg-config.component';
 import { PgConfigRoutingModule } from './pg-config-routing.module';
-import { PgConfigEpsModule } from './pg-config-eps/pg-config-eps.module';
 import { PgConfigServicesModule } from './pg-config-services/pg-config-services.module';
 import { CellphonePipe } from 'src/app/pipes/cellphone.pipe';
 import { PipesModule } from 'src/app/pipes/pipes.module';
@@ -18,8 +17,6 @@ import { CtnEpsModule } from './ctn-eps/ctn-eps.module';
   imports: [
     CommonModule,
     PgConfigRoutingModule,
-    PgConfigEpsModule,
-    PgConfigServicesModule,
     PipesModule,
     CtnProfileModule,
     CtnEpsModule
